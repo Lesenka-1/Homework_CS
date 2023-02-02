483793c34b990aef3b1b6d6b227cf9e0a16ff54a
alesia <albenderina@mail.ru>
2023-02-02 23:04:07 +0300

    Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 38: Задайте массив целых чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] array = new int[new Random().Next(2,11)];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100,101);
+    Console.Write($"{array[i]}, ");
+}
+Console.WriteLine("\b\b ");
+
+int min = 0;
+int max = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+Console.WriteLine($"разница между максимальным ({max}) и минимальным ({min}) элементом массива равна {max - min}");
